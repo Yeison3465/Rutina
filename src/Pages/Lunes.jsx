400a3f4006ced42a8ed86e6d71f5057190e105a0
yeison <yeisonarrieta0@gmail.com>
2025-06-03 19:46:19 -0500

    un retoque nuevos

--- src/Pages/Lunes.jsx
+++ src/Pages/Lunes.jsx
@@ -11,14 +11,14 @@ export const Lunes = () => {
             <Mensaje Dia="Lunes" />
             <div className="max-w-6xl mx-auto px-6 py-10 grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
                 <Cards numero={1} video="Assets/Videos/Lunes/0523.mp4" Serie={3}  Repeticion={15}/>
-                <Cards numero={2} video="public/Videos/Lunes/0524.mp4" Serie={3} Repeticion={20}/>
+                {/* <Cards numero={2} video="public/Videos/Lunes/0524.mp4" Serie={3} Repeticion={20}/>
                 <Cards numero={3} video="public/Videos/Lunes/0525.mp4" Serie={3} Repeticion={10} />
                 <Cards numero={4} video="public/Videos/Lunes/0526.mp4" Serie={2} Repeticion={8}/>
                 <Cards numero={5} video="public/Videos/Lunes/0527.mp4" Serie={3} Repeticion={10}/>
                 <Cards numero={6} video="public/Videos/Lunes/0528.mp4" Serie={3} Repeticion={20}/>
                 <Cards numero={7} video="public/Videos/Lunes/0551.mp4" Serie={2} Repeticion={7}/>
                 <Cards numero={8} video="public/Videos/Lunes/0553.mp4" Serie={2} Repeticion={8}/>
-                <Cards numero={9} video="public/Videos/Lunes/0555.mp4" Serie={2} Repeticion={7}/>
+                <Cards numero={9} video="public/Videos/Lunes/0555.mp4" Serie={2} Repeticion={7}/> */}
             </div>
         </div>
     );
